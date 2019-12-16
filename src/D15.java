da5c8eaa4c41057bb63def6722acb31c4d6d0e6e
Sten Laane <sten.laane@kcl.ac.uk>
2019-12-16 22:47:34 +0200

    Day 15

--- src/D15.java
+++ src/D15.java
@@ -1,8 +1,11 @@
 import helper.Helper;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public abstract class DaySkeleton {
+public class D15 {
 
     public static void main(String[] args) {
         q1();
@@ -10,15 +13,101 @@ public abstract class DaySkeleton {
     }
 
     public static void q1() {
-        List<String> input = Helper.test();
-//        input = Helper.input();
+        List<String> input = Helper.input(15);
+        List<Long> codes = Arrays.stream(input.get(0).split(","))
+            .map(Long::parseLong)
+            .collect(Collectors.toList());
+
+        moveHelper1(new Computer(codes), 1, 1);
+        moveHelper1(new Computer(codes), 2, 1);
+        moveHelper1(new Computer(codes), 3, 1);
+        moveHelper1(new Computer(codes), 4, 1);
+
+    }
+
+    public static void moveHelper1(Computer computer, long move, int depth) {
+        Computer computerCopy = new Computer(new ArrayList<>(computer.memory));
+        computerCopy.programCounter = computer.programCounter;
+        computerCopy.base = computer.base;
+        long output = computerCopy.run(move);
+
+        if (output == 1) {
+            List<Long> moves = List.of(1L, 2L, 3L, 4L);
+            List<Long> validMoves = new ArrayList<>(moves);
+            validMoves.removeIf(potentialMove -> {
+                if (move == 1 && potentialMove == 2) return true;
+                if (move == 2 && potentialMove == 1) return true;
+                if (move == 3 && potentialMove == 4) return true;
+                if (move == 4 && potentialMove == 3) return true;
+                return false;
+            });
+
+            for (Long validMove : validMoves) {
+                moveHelper1(computerCopy, validMove, depth + 1);
+            }
+        } else if (output == 2) {
+            System.out.println(depth);
+        }
 
     }
 
     public static void q2() {
-        List<String> input = Helper.test();
-//        input = Helper.input();
+        List<String> input = Helper.input(15);
+        List<Long> codes = Arrays.stream(input.get(0).split(","))
+            .map(Long::parseLong)
+            .collect(Collectors.toList());
+
+        List<Integer> moves = new ArrayList<>();
+        moveHelper2(new Computer(codes), 2, 1, false, moves);
+        System.out.println(moves.stream().max(Integer::compareTo).get());
     }
+
+    public static void moveHelper2(Computer computer, long move, int depth, boolean returning, List<Integer> movesToDeadEnd) {
+        Computer computerCopy = new Computer(new ArrayList<>(computer.memory));
+        computerCopy.programCounter = computer.programCounter;
+        computerCopy.base = computer.base;
+        long output = computerCopy.run(move);
+
+        if (output == 1) {
+            List<Long> moves = List.of(1L, 2L, 3L, 4L);
+            List<Long> validMoves = new ArrayList<>(moves);
+            validMoves.removeIf(potentialMove -> {
+                if (move == 1 && potentialMove == 2) return true;
+                if (move == 2 && potentialMove == 1) return true;
+                if (move == 3 && potentialMove == 4) return true;
+                if (move == 4 && potentialMove == 3) return true;
+                return false;
+            });
+
+            List<Long> outputs = new ArrayList<>();
+            for (Long validMove : validMoves) {
+                outputs.add(tryMove(computerCopy, validMove));
+            }
+            if (outputs.stream().allMatch(o -> o == 0) && returning) {
+                movesToDeadEnd.add(depth);
+            }
+
+            for (Long validMove : validMoves) {
+                moveHelper2(computerCopy, validMove, depth + 1, returning, movesToDeadEnd);
+            }
+
+        } else if (output == 2) {
+            long validMove = 0;
+            if (move == 1)  validMove = 2;
+            if (move == 2)  validMove = 1;
+            if (move == 3)  validMove = 4;
+            if (move == 4)  validMove = 3;
+            moveHelper2(computerCopy, validMove, 1, true, movesToDeadEnd);
+        }
+    }
+
+    public static long tryMove(Computer computer, long move) {
+        Computer computerCopy = new Computer(new ArrayList<>(computer.memory));
+        computerCopy.programCounter = computer.programCounter;
+        computerCopy.base = computer.base;
+        return computerCopy.run(move);
+    }
+
 
     public static class Computer {
         public List<Long> memory;
@@ -36,7 +125,7 @@ public abstract class DaySkeleton {
             base = 0;
         }
 
-        public long run() {
+        public long run(long input) {
             for (;programCounter < memory.size();) {
                 long code = memory.get(programCounter);
                 StringBuilder s = new StringBuilder(String.valueOf(code));
@@ -53,7 +142,7 @@ public abstract class DaySkeleton {
                     long par1 = getParAlt(par1mode, 1);
 
 
-                    long input = 0; // processing of actual input goes here
+//                    long input = 0; // processing of actual input goes here
 
 
                     memory.set((int) par1, input);
@@ -63,9 +152,10 @@ public abstract class DaySkeleton {
 
 
 //                    System.out.println(par1); // processing of actual output goes here
-
-
                     programCounter += 2;
+                    return par1;
+
+
                 } else if (opcode == 5) { // branch if not zero
                     long par1 = getPar(par1mode, 1);
                     long par2 = getPar(par2mode, 2);
@@ -80,7 +170,7 @@ public abstract class DaySkeleton {
                     base += par;
                     programCounter += 2;
                 } else if (opcode == 99) { // halt
-                        break;
+                    break;
                 } else {
                     long par1 = getPar(par1mode, 1);
                     long par2 = getPar(par2mode, 2);
